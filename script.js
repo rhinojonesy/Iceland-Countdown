7322bf0dd85abaeac00cd7885585ee16bb10b27f
rhinojonesy <124844071+rhinojonesy@users.noreply.github.com>
2023-08-03 16:14:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-// Set the target date and time for the countdown (e.g., April 18, 2024, 12:00:00 UTC)
-const targetDate = new Date("2024-04-18T12:00:00Z").getTime();
+// Set the date for the countdown in "YYYY-MM-DD" format
+const countdownDate = new Date("2024-04-18").getTime();
 
 // Function to update the countdown
 function updateCountdown() {
   const now = new Date().getTime();
-  const timeRemaining = targetDate - now;
+  const timeRemaining = countdownDate - now;
 
-  // Calculate days, hours, minutes, and seconds
   const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
   const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-  // Display the countdown
-  const countdownElement = document.getElementById("countdown");
-  countdownElement.innerHTML = `Countdown: ${days}d ${hours}h ${minutes}m ${seconds}s`;
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours.toString().padStart(2, '0');
+  document.getElementById("minutes").innerText = minutes.toString().padStart(2, '0');
+  document.getElementById("seconds").innerText = seconds.toString().padStart(2, '0');
 
-  // If the countdown has reached zero, display a message
+  // If the countdown is over, display 00:00:00:00
   if (timeRemaining < 0) {
-    countdownElement.innerHTML = "Countdown has ended!";
+    clearInterval(countdownInterval);
+    document.getElementById("days").innerText = '00';
+    document.getElementById("hours").innerText = '00';
+    document.getElementById("minutes").innerText = '00';
+    document.getElementById("seconds").innerText = '00';
   }
 }
 
-// Call the updateCountdown function every second to keep the countdown updated
-setInterval(updateCountdown, 1000);
+// Call the updateCountdown function every second
+const countdownInterval = setInterval(updateCountdown, 1000);
 
-// Initial call to update the countdown when the page loads
+// Initial call to avoid initial delay
 updateCountdown();
